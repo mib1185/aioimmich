dadf1de2747982da7062b14f439cd462e0c4658a
Michael <35783820+mib1185@users.noreply.github.com>
2025-05-19 22:51:11 +0200

    Add video stream support (#9)

--- aioimmich/assets/__init__.py
+++ aioimmich/assets/__init__.py
@@ -1,5 +1,7 @@
 """aioimmich assets api."""
 
+from aiohttp import StreamReader
+
 from ..api import ImmichApi
 
 
@@ -24,4 +26,21 @@ class ImmichAssests:
             f"assets/{asset_id}/thumbnail", {"size": size}, application="octet-stream"
         )
         assert isinstance(result, bytes)
+        return result
+
+    async def async_play_video_stream(self, asset_id: str) -> StreamReader:
+        """Get a video stream.
+
+        Arguments:
+            asset_id (str)  id of the video to be streamed
+
+        Returns:
+            the video stream as `StreamReader`
+        """
+        result = await self.api.async_do_request(
+            f"assets/{asset_id}/video/playback",
+            application="octet-stream",
+            raw_response_content=True,
+        )
+        assert isinstance(result, StreamReader)
         return result
