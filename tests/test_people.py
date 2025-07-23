1727bde69ac9e2ef90eeceb5a8fde8dd00d4fa9e
Michael <35783820+mib1185@users.noreply.github.com>
2025-07-23 19:06:18 +0200

    Fix defaults in async_get_all_people (#30)
    * use sane defaults in async_get_all_people
    
    * fix tests

--- tests/test_people.py
+++ tests/test_people.py
@@ -8,7 +8,7 @@ from syrupy.assertion import SnapshotAssertion
 async def test_get_all_people(mock_immich_with_data, snapshot: SnapshotAssertion):
     """Test async_get_all_tags."""
     api = await mock_immich_with_data()
-    people = await api.people.async_get_all_people()
+    people = await api.people.async_get_all_people(page_size=3, max_pages=1)
 
     assert len(people) == 3
     assert people == snapshot
