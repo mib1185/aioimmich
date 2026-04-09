e39e7c499b688d510b9cedddceb9be3d9b9ae780
Michael <35783820+mib1185@users.noreply.github.com>
2026-04-09 20:50:52 +0200

    Add get favorites function (#49)

--- tests/test_search.py
+++ tests/test_search.py
@@ -22,6 +22,23 @@ async def test_get_all_assets(
     assert assets == snapshot
 
 
+async def test_get_all_favorite_assets(
+    mock_immich_with_data, mock_aioresponse, snapshot: SnapshotAssertion
+):
+    """Test async_get_all_favorites."""
+    api = await mock_immich_with_data()
+    assets = await api.search.async_get_all_favorites()
+    mock_aioresponse.assert_called_with(
+        "https://localhost:2283/api/search/metadata",
+        "post",
+        headers={"Accept": "application/json", "x-api-key": "abcdef1234567890"},
+        params=None,
+        json={"size": 100, "isFavorite": True, "page": 1},
+    )
+    assert len(assets) == 4
+    assert assets == snapshot
+
+
 async def test_get_all_by_tag_ids(
     mock_immich_with_data, mock_aioresponse, snapshot: SnapshotAssertion
 ):
